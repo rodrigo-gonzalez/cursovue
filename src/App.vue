8495d2999d772f701e584c6964f27c8874c04c8f
RODRIGO ANDRES GONZALEZ <rgonzalezar@contraloria.cl>
2018-04-11 15:13:40 -0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:artist="country.value") {{ country.name }}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bin:key="artist.mbid") 
+</template>
+
+<script>
+import Spinner from './components/Spinner.vue'
+import Artist from './components/Artist.vue'
+import getArtists from './api'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists: [],
+      selectedCountry: 'Argentina',
+      loading: true,
+      countries: [ 
+        {name: 'Argentina', value: 'Argentina'},
+        {name: 'Colombia', value: 'Colombia'},
+        {name: 'España', value: 'Spain'}
+      ]
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtists: function() {
+      const self = this
+      this.loading = true
+      getArtists(this.selectedCountry)
+        .then(function(artists) {
+          self.loading = false
+          self.artists = artists
+        })
+    }
+  },
+  mounted: function() {
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry: function() {
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: 'af143f061e6a86a60c81b94fcf7462b0',
+	secret: '1c4e3479d8f02b58596da38d883a3c73',
+	registeredTo: 'r0gnark'
+}
+
+//Application name	platzimusic
+//API key	af143f061e6a86a60c81b94fcf7462b0
+//Shared secret	1c4e3479d8f02b58596da38d883a3c73
+//Registered to	r0gnark
